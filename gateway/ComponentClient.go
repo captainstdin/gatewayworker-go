4b281cd76edb94e47a911fc5c3751f51e8f1bf46
tricky <admin@qqfirst.com>
2023-06-19 17:29:52 +0800

    1: gateway 编写启动函数

--- gateway/ComponentClient.go
+++ gateway/ComponentClient.go
@@ -2,28 +2,15 @@ package gateway
 
 import (
 	"gatewaywork-go/workerman_go"
-	"net"
 )
 
 // ComponentClient 每个连接上来的ws Client主要是  component组件(business)与 WebSocket用户
 type ComponentClient struct {
-	ClientId string
-	//是否是用户,true是用户，
-	IsClient bool
+	ClientId *workerman_go.ClientToken
 
-	//session 设置
-	Session *workerman_go.SessionKv
-
-	//ip类型
-	IpType workerman_go.IpType
-
-	//ip4
-	Ipv4 net.IP
-
-	//ip6
-	Ipv6 net.IP
-
-	Port workerman_go.Port
+	//连接地址
+	Address string
+	Port    workerman_go.Port
 
 	//生成的在当前内部组件中标志目标gateway所在地
 	ClientToken *workerman_go.ClientToken
--- gateway/GatewayServer.go
+++ gateway/GatewayServer.go
@@ -1,49 +1,91 @@
 package gateway
 
 import (
+	"crypto/tls"
 	"gatewaywork-go/workerman_go"
+	"golang.org/x/net/websocket"
+	"log"
+	"net"
+	"net/url"
 	"sync"
+	"time"
 )
 
 // GatewayServer 需要有公网端口2727，那些人会连2727？{Sdk:http://:2727/action,Business:ws://:2727, Client:ws://:2727}
 type GatewayServer struct {
 	//监听地址，可以是:8080，同时监听ipv6 4
 	ListenAddr string
-	//(非必填)启动服务器
-	OnWorkerStart func(Worker *GatewayServer)
-
-	//(非必填)Sdk || Business || Client 连接上来
-	OnConnect func(conn workerman_go.TcpConnection)
-	//(非必填)SSdk || Business || Client 发送指令
-	OnMessage func(Worker workerman_go.TcpConnection, msg []byte)
-
-	//(非必填)SSdk || Business || Client 断开连接
-	OnClose func(Worker workerman_go.TcpConnection)
 
 	//服务配置
-	GatewayWorkerConfig workerman_go.ConfigGatewayWorker
+	Config workerman_go.ConfigGatewayWorker
 
-	//记录了 Sdk || Business || Client
-	ConnectionList map[uint64]*workerman_go.TcpConnection
+	//记录了 Client
+	ConnectionMap map[uint64]*workerman_go.TcpConnection
 	//读写锁
-	ConnectionListRWLock *sync.RWMutex
+	ConnectionMapRWLock *sync.RWMutex
+
+	ComponentClient map[uint64]*ComponentClient
+
+	Register map[uint64]*ComponentClient
 
 	Name string
 }
 
 func (g *GatewayServer) Run() error {
-	//todo 1-1：开启监听本地ws://:2727 ,http://:2727
+
+	g.onWorkerStart(g)
 
 	//todo 1-2: http://启动一个gin服务器，
 
-	g.RunGinServer(g.GatewayWorkerConfig.GatewayListenAddr, g.GatewayWorkerConfig.GatewayListenPort)
+	g.RunGinServer(g.Config.GatewayListenAddr, g.Config.GatewayListenPort)
 
 	//todo 2：准备完毕，连接Register，并且
 	return nil
 }
 
-// InnerOnWorkerStart 内置服务启动回调
-func (g *GatewayServer) InnerOnWorkerStart(worker workerman_go.Worker) {
+// onWorkerStart 连接到register注册中心
+func (g *GatewayServer) onWorkerStart(worker *GatewayServer) {
+	//TODO implement me
+
+	Scheme := "wss"
+	if !g.Config.TLS {
+		Scheme = "ws"
+	}
+
+	// 设置WebSocket客户端配置
+	wsConfig := &websocket.Config{
+		Location: &url.URL{
+			Scheme: Scheme,
+			Host:   g.Config.RegisterPublicHostForComponent,
+			Path:   workerman_go.RegisterForBusniessWsPath,
+		},
+		Dialer: &net.Dialer{
+			Timeout: 10 * time.Second,
+		},
+		TlsConfig: &tls.Config{InsecureSkipVerify: g.Config.SkipVerify},
+		Version:   websocket.ProtocolVersionHybi13,
+		Origin:    &url.URL{Scheme: "http", Host: g.Config.RegisterPublicHostForComponent},
+	}
+
+	g.com.Lock()
+
+	wsRegister, wsConnWithRegisterErr := websocket.DialConfig(wsConfig)
+
+	if wsConnWithRegisterErr != nil {
+		for wsConnWithRegisterErr != nil {
+			wsRegister, wsConnWithRegisterErr = websocket.DialConfig(wsConfig)
+
+			log.Println(wsConnWithRegisterErr)
+			log.Printf("[%s]无法连接  注册发现 {%s://%s%s}  10秒后重连.. ", b.Name, Scheme, b.Config.RegisterPublicHostForComponent, workerman_go.RegisterForBusniessWsPath)
+			t := time.NewTicker(time.Second * 10)
+			<-t.C
+		}
+	}
+
+}
+
+// OnWorkerStart 内置服务启动回调
+func (g *GatewayServer) OnWorkerStart(worker workerman_go.Worker) {
 	//TODO implement me
 	panic("implement me")
 }
